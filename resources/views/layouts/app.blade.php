48eed3eabccfad2f746ab2aadd76aea08ca083db
David Taylor <2797712@qq.com>
2020-04-29 17:50:56 +0800

    upgrade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="zh-Hans">
 <head>
     <meta charset="utf-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge, chrome=1">
     <title>Welcome</title>
     <link rel="stylesheet" href="//cdn.staticfile.org/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/assets/1.0.0/css/app.css">
-    <link rel="stylesheet" href="/static/css/signin.css">
+    <link rel="stylesheet" href="/static/css/app.css">
 </head>
 <body>
 <div class="container">
@@ -17,6 +16,6 @@
 <script src="//cdn.staticfile.org/jquery/3.4.1/jquery.min.js"></script>
 <script src="//cdn.staticfile.org/vue/2.6.10/vue.min.js"></script>
 <script src="//cdn.staticfile.org/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
-<script src="/assets/1.0.0/js/app.js"></script>
+<script src="/static/js/app.js"></script>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,7 @@
-<!DOCTYPE html>
-<html lang="zh-Hans">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Welcome</title>
-</head>
-<body>
-{{ $message }}
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    <div class="content">
+        Welcome {{ $message }}
+    </div>
+@endsection
