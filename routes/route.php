815549e92dafb828769eef86206eb2bc9a42b1ec
David Taylor <2797712@qq.com>
2020-05-26 17:44:21 +0800

    upgrade

--- routes/route.php
+++ routes/route.php
@@ -4,4 +4,4 @@ declare(strict_types=1);
 
 use Hyperf\HttpServer\Router\Router;
 
-Router::get('/[{path}]', 'App\Http\Controller\Web\IndexController@view');
+Router::get('/[{path}]', 'App\Controller\Web\IndexController@view');
